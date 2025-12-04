b5483d5b063914cf44191fd8392f5f192cb80fdd
ca1hua <143733039+ca1hua@users.noreply.github.com>
2025-12-04 09:35:10 +0800

    Add files via upload

---
+++ python考试/stepbystep.py
@@ -0,0 +1,18 @@
+changeOne = int(input())
+changeTwo = int(input())
+plus = int(input())
+
+# 请在此添加代码，交换changeOne、changeTwo的值，然后计算changeOne、plus的和result的值
+########## Begin ##########
+
+swap = changeOne
+changeOne = changeTwo
+changeTwo = swap
+
+
+result = changeOne + plus
+
+
+
+########## End ##########
+print(result)
